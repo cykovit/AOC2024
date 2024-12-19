9eac7c5086b4f58250a9e284e6b3e00a27e0e8ef
cyko <142172153+cykovit@users.noreply.github.com>
2024-12-19 18:40:53 +0100

    Create aoc2024_day19_part1.py

---
+++ day19/aoc2024_day19_part1.py
@@ -0,0 +1,17 @@
+patterns, designs = open('input_day19.txt').read().split('\n\n')
+
+memo = {}
+
+def fill(d):
+    if d in memo:
+        return memo[d]
+    if d == '':
+        result = 1
+    elif len(v := [p for p in patterns.split(', ') if d.startswith(p)]) == 0:
+        result = 0
+    else:
+        result = sum([fill(d[len(p):]) for p in v])
+    memo[d] = result
+    return result
+
+print(sum(fill(d) > 0 for d in designs.splitlines()))
